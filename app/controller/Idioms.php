6905a5f5da3b0a53de19b6485d0e07b04fee84bb
Max_call <kefu@andsmind.com>
2020-11-10 19:22:14 +0800

    change4

--- app/controller/Idioms.php
+++ app/controller/Idioms.php
@@ -11,12 +11,8 @@ class Idioms extends BaseController
 {
     public function search()
     {
-
         $url = 'http://v.juhe.cn/chengyu/query?word=' . urlencode(input("get.word")) ."&dtype=json&key=a589b5fc927f8ee8a697ac1ede722de8" ;
-
         return json( file_get_contents($url));
-
-//        return View::fetch('index');
     }
 
 }
