4f9bdf6e083c97ce8b6205554b922b22781e7537
xiaorui293 <desenyang@andsmind.com>
2020-11-10 17:33:09 +0800

    change1

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -11,14 +11,65 @@ class Index extends BaseController
 {
     public function index()
     {
-//        $user =  Db::table("tp_admin")->select();
-////        return 'hello,' ."basepath ".$this->app->getBasePath();
-//        return json($user);
-//        $this->display("Index/index");
-        $info = Db::table('tp_numbers')->order("id des")->limit(1)->select();
-        View::assign('data',$info);
+        if(Session::get("logined") == true){
 
-        return View::fetch('index');
+            if(input("?get.search")) {
+                $info = Db::table('tp_numbers')->where("qishu",input("get.search"))->select();
+                View::assign('total', sizeof($info));
+                View::assign('totalpage', 1);
+
+                View::assign('info', $info);
+                return View::fetch('index');
+            }
+            else{
+                $info = Db::table('tp_numbers')->select();
+                $total = Db::table("tp_numbers")->count();
+                View::assign('total', $total);
+                View::assign('info', $info);
+                return View::fetch('index');
+            }
+
+        }else{
+            View::assign('name','ThinkPHP');
+            return View::fetch('login');
+        }
+    }
+
+    public function login (Request  $request) {
+        if(input('?post.username') && input("?post.password")) {
+            $username =  input('post.username');
+            $password =   input('post.password');
+            $realinfo  = Db::table("tp_admin")->where("id",1)->find();
+            $name = $realinfo["username"];
+            $pass = $realinfo["password"];
+            if($username == $name &&  password_verify($password,$pass)){
+                Session::set("logined",true);
+                return redirect("/");
+            }else{
+//                return password_hash("123",PASSWORD_DEFAULT);
+                return $username ."::".$password .": 登录错误";
+            }
+        }else{
+            return "camsji: 登录错误";
+        }
+    }
+    public function resetpass () {
+        if(Session::get("logined") == true){
+            if( input("?post.password")) {
+                $pass = input("post.password");
+                Session::set("logined",false);
+                Db::table("tp_admin")->update(["id"=>1,"password"=>password_hash($pass,PASSWORD_DEFAULT)]);
+                return json(["code"=>1]);
+            }else{
+                return json(["code"=>2]);
+            }
+        }
+        return json(["code"=>0]);
+    }
+    public function logout () {
+        Session::set("logined",false);
+        return json(["code"=>1]);
+
     }
 
 }
